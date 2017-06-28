83bc9c622d9d7d177aeeb94b508ea29ff7e18c62
sergiohgsp <sergiohenrique.g.s.p@gmail.com>
2017-06-28 08:47:58 -0300

    deupau
    ta com pau

---
+++ quickstart-master/src/app/app.component.js
@@ -0,0 +1,20 @@
+"use strict";
+var AppComponent = (function () {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
+        this.title = 'Tour of Heroes';
+    }
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
